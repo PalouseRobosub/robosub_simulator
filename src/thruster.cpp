3d09f539f0407e26b858780dce24de91797f0ef1
Dustin Crossman <dustin.crossman@gmail.com>
2016-11-12 23:39:35 -0800

    Making some progress on thruster plugin.

--- src/thruster.cpp
+++ src/thruster.cpp
@@ -10,6 +10,7 @@ Thruster::~Thruster() { }
 void Thruster::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 {
     this->model = _parent;
+    received_msg = false;
 
     int argc = 0;
     char **argv = NULL;
@@ -22,8 +23,22 @@ void Thruster::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
         return;
     }
 
-    nh = new ros::NodeHandle("thruster");
-    pub = nh->advertise<std_msgs::String>("/thruster", 1);
+    XmlRpc::XmlRpcValue thruster_settings;
+    if(!ros::param::get("thrusters", thruster_settings))
+    {
+        ROS_FATAL("thruster params failed to load");
+        exit(1);
+    }
+
+    num_thrusters = 0;
+    for(int i=0; i < thruster_settings.size(); ++i)
+    {
+        thruster_names.push_back(thruster_settings[i]["name"]);
+        num_thrusters++;
+    }
+
+    nh = new ros::NodeHandle();
+	thruster_sub = nh->subscribe("thruster", 1, &Thruster::thrusterCallback, this);
 
     updateConnection = event::Events::ConnectWorldUpdateBegin(
             boost::bind(&Thruster::UpdateChild, this));
@@ -34,13 +49,30 @@ void Thruster::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 void Thruster::UpdateChild()
 {
     // Add force directly to thrusters?
-    physics::LinkPtr t = model->GetLink("dive_front_left");
-    t->AddLinkForce(math::Vector3(0.0, 0.0, 100.0));
+    // Wait for first message
+    if(received_msg)
+    {
+        double max_thrust;
+        nh->getParamCached("max_thrust", max_thrust);
+        for(int i=0; i < num_thrusters; i++)
+        {
+            physics::LinkPtr t = model->GetLink(thruster_names[i]);
+            t->AddLinkForce(math::Vector3(0.0, 0.0, last_thruster_msg.data[i]*max_thrust));
+            ROS_INFO_STREAM(thruster_names[i] << ": " << last_thruster_msg.data[i]);
+        }
+    }
 
-    // or do it this way
-    physics::LinkPtr frame = model->GetLink("frame");
+    // or do it this way?
+    //physics::LinkPtr frame = model->GetLink("frame");
     //foreach thruster
-    //t->AddLinkForce(math::Vector3(0.0, 0.0, 0.0), math::Vector3(thruster coords));
+    //t->AddLinkForce(math::Vector3(thruster force), math::Vector3(thruster coords));
+    //remember that thruster force must take into account thruster orientation
+}
+
+void Thruster::thrusterCallback(const robosub::thruster::ConstPtr& msg)
+{
+    received_msg = true;
+    last_thruster_msg.data = msg->data;
 }
 
 GZ_REGISTER_MODEL_PLUGIN(Thruster)
