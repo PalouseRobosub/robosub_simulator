c2470f9d1e450e0945188cad09a37b721c19402a
Dustin Crossman <dustin.crossman@gmail.com>
2016-11-12 22:38:14 -0800

    Added thrusters to sub model. Enabled thruster plugin for sub. Thruster plugin itself WIP.

--- src/thruster.cpp
+++ src/thruster.cpp
@@ -22,19 +22,25 @@ void Thruster::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
         return;
     }
 
-    ROS_INFO_STREAM("Hello");
-
     nh = new ros::NodeHandle("thruster");
     pub = nh->advertise<std_msgs::String>("/thruster", 1);
 
     updateConnection = event::Events::ConnectWorldUpdateBegin(
             boost::bind(&Thruster::UpdateChild, this));
+
+    ROS_INFO_STREAM("Thruster plugin initialized");
 }
 
 void Thruster::UpdateChild()
 {
-    math::Vector3 vel(0.1, 0.0, 0.0);
-    model->SetLinearVel(vel);
+    // Add force directly to thrusters?
+    physics::LinkPtr t = model->GetLink("dive_front_left");
+    t->AddLinkForce(math::Vector3(0.0, 0.0, 100.0));
+
+    // or do it this way
+    physics::LinkPtr frame = model->GetLink("frame");
+    //foreach thruster
+    //t->AddLinkForce(math::Vector3(0.0, 0.0, 0.0), math::Vector3(thruster coords));
 }
 
 GZ_REGISTER_MODEL_PLUGIN(Thruster)
