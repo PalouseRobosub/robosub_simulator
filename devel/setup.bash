7cc275abd5ff0715bf7c2734cd9dba6b316557f7
Brandon Kallaher <brandonj@kallaher.org>
2016-12-06 16:38:40 -0800

    Better Model Path Settings through the package.xml file

--- devel/setup.bash
+++
@@ -1,5 +0,0 @@
-# Source this file from your bashrc
-source /usr/share/gazebo-7/setup.sh
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(rospack find simulator_gazebo)/models
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:$(rospack find simulator_gazebo)/../../devel/lib
-export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:$(rospack find simulator_gazebo)/models
