d39e4b0d8be35d5a3677076e4edc8103629afd59
Dustin Crossman <dustin.crossman@gmail.com>
2016-12-07 00:42:12 -0800

    Positions, orientations, and sizes all corrected. Side A gate, dock, and buoys in place.

--- scripts/spawn_robosub.sh
+++ scripts/spawn_robosub.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosrun gazebo_ros spawn_model -file $(rospack find robosub_simulator)/models/cobalt/model.sdf -sdf -x 0 -y 0 -z 0.1 -model robosub
+rosrun gazebo_ros spawn_model -file $(rospack find robosub_simulator)/models/cobalt/model.sdf -sdf -x 7.0 -y -26.2 -z -1.2 -Y 0.8 -model robosub
