433517729a95a97eab1f3f2c24f998d9350c8a06
James Irwin <irwineffect@gmail.com>
2016-12-07 11:45:00 -0800

    tweaked launchfile so everything launches, moved simulator bridge from main repo

--- scripts/spawn_robosub.sh
+++ scripts/spawn_robosub.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosrun gazebo_ros spawn_model -file $(rospack find simulator_gazebo)/models/cobalt/model.sdf -sdf -x 0 -y 0 -z 0.1 -model robosub
+rosrun gazebo_ros spawn_model -file $(rospack find robosub_simulator)/models/cobalt/model.sdf -sdf -x 0 -y 0 -z 0.1 -model robosub
