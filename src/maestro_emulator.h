d0b89bc3fb327324b1a542fc2575cccea7a48762
Ryan Summers <summers.ryan.m@gmail.com>
2017-01-28 00:48:26 -0800

    Cleaned up maestro emulator class

--- src/maestro_emulator.h
+++ src/maestro_emulator.h
@@ -7,11 +7,32 @@
 
 using std::string;
 
+/**
+ * MaestroEmulator class.
+ *
+ * @note This class is used to emulate the behavior of the Maestro motor
+ *       controller. Beware that this driver class only supports the command
+ *       for setting a single maestro channel. Additionally, it is configured
+ *       specifically for the BlueRobotics T200 thruster parameters.
+ *       Note additionally that all force values internally are in KgF
+ *       (kilogram force), but all external outputs are in Newtons.
+ */
 class MaestroEmulator
 {
+    /*
+     * Conversion constant for converting KgF (Kilogram force) to N.
+     */
     static constexpr double _kgf_to_newtons = 9.80665;
+
+    /*
+     * The minimum thrust output by the T200 thruster (in KgF).
+     */
     static constexpr double _minimum_thrust_kgf = 0.01;
 
+    /**
+     * The current state of the serial port commands. This class allows for the
+     * update function to terminate in the middle of a command sequence.
+     */
     enum class State
     {
         None,
@@ -21,7 +42,7 @@ class MaestroEmulator
 
 public:
     MaestroEmulator() :
-        _port(nullptr),
+        _port(),
         _thruster_speeds(),
         _thruster_timeouts(),
         _channel_names(),
@@ -37,13 +58,51 @@ public:
     double getThrusterForce(string name);
 
 private:
-    rs::Serial *_port;
+    /*
+     * The virtual serial port to read from.
+     */
+    rs::Serial _port;
+
+    /*
+     * A hashmap of thruster speeds indexed by the name of the thruster.
+     */
     std::map<string, int> _thruster_speeds;
+
+    /*
+     * A hashmap of the next thruster timeouts indexed by the name of the
+     * thruster.
+     */
     std::map<string, ros::Time> _thruster_timeouts;
+
+    /*
+     * A hashmap of the names of a given maestro channel (as specified in the
+     * settings) into a specific thruster name.
+     */
     std::map<int, string> _channel_names;
+
+    /*
+     * Specifies whether or not the emulator has been initialized. No
+     * functionality is permitted until initialization has occurred.
+     * Initialization may only occur once.
+     */
     bool _is_initialized;
+
+    /*
+     * Specifies whether or not the emulator has received the beginning
+     * baud-detect setinel character from the serial port (0xAA).
+     */
     bool _is_connected;
+
+    /*
+     * Specifies the current, asynchronous state of the thruster commands read
+     * from the serial port.
+     */
     State _current_state;
+
+    /*
+     * Specifies which thruster the next data read off the serial port is
+     * destined for.
+     */
     string _thruster;
 
     /*
