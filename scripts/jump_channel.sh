20718d0e031938855507626e06db5d99f32f9383
James Irwin <irwineffect@gmail.com>
2017-02-08 14:48:20 -0800

    added scripts to jump the sub around the pool

---
+++ scripts/jump_channel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# this script will jump the sub in the simulator
+rosservice call /gazebo/set_model_state "model_state:
+  model_name: 'robosub'
+  pose:
+    position:
+      x: 26
+      y: -16.9
+      z: -2.7
+    orientation:
+      x: 0.0
+      y: 0.0
+      z: 0.54
+      w: 0.84"
+rostopic pub --once /control robosub/control "
+forward_state: 0
+strafe_state: 0
+dive_state: 1
+roll_state: 1
+pitch_state: 1
+yaw_state: 1
+forward: 0.0
+strafe_left: 0.0
+dive: -1.8
+roll_right: 0.0
+pitch_down: 0.0
+yaw_left: 64"
