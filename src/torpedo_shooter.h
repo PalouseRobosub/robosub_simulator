b7b174a9de2ab0ba4948913dbc5c29e8f1f68e4b
Peter Brennan <peter.brennan@wsu.edu>
2017-04-12 17:59:38 -0700

    Made src files for the torpedo shooter based on the marker dropper files

---
+++ src/torpedo_shooter.h
@@ -0,0 +1,43 @@
+#ifndef TORPEDO_SHOOTER_H
+#define TORPEDO_SHOOTER_H
+
+#include <ignition/math/Pose3.hh>
+
+#include <boost/bind.hpp>
+
+#include "gazebo/physics/physics.hh"
+#include "gazebo/common/common.hh"
+#include <gazebo/transport/transport.hh>
+#include "gazebo/gazebo.hh"
+
+#include "ros/ros.h"
+#include <ros/callback_queue.h>
+#include <ros/advertise_options.h>
+#include <std_msgs/String.h>
+#include "std_srvs/Empty.h"
+
+#include <iostream>
+
+namespace gazebo
+{
+class TorpedoShooter : public ModelPlugin
+{
+  private:
+    event::ConnectionPtr updateConnection;
+    ros::NodeHandle *nh;
+    ros::Publisher pub;
+    ros::CallbackQueue service_callback_queue;
+    ros::ServiceServer service_server;
+    physics::ModelPtr sub;
+    physics::WorldPtr world;
+
+    bool drop(std_srvs::Empty::Request  &req, std_srvs::Empty::Response &res);
+
+  public:
+    TorpedoShooter() {}
+    ~TorpedoShooter() {}
+    void Load(physics::ModelPtr _parent, sdf::ElementPtr);
+    void Update();
+};
+}
+#endif // TORPEDO_SHOOTER_H
