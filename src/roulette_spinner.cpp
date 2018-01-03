19e9e530c005b57838c896dc44d78ed29a7b7460
Ryan Summers <summers.ryan.m@gmail.com>
2018-01-02 19:39:31 -0800

    Added in roulette spinning plugin

---
+++ src/roulette_spinner.cpp
@@ -0,0 +1,35 @@
+#include "roulette_spinner.h"
+#include <ros/ros.h>
+
+namespace gazebo
+{
+void RouletteSpinner::Load(physics::ModelPtr _parent, sdf::ElementPtr sdf)
+{
+    roulette_wheel = _parent;
+
+    updateConnection = event::Events::ConnectWorldUpdateBegin(
+    boost::bind(&RouletteSpinner::Update, this));
+
+    if (sdf)
+    {
+        rpm = sdf->Get<float>("rate");
+    }
+    else
+    {
+        rpm = 1;
+    }
+
+    ROS_INFO_STREAM("RouletteSpinner plugin loaded. Spinning at " << rpm << " RPM");
+}
+
+void RouletteSpinner::Update()
+{
+    physics::LinkPtr face = roulette_wheel->GetChildLink("roulette_wheel");
+
+    math::Vector3 angular_velocity(0, 0, 2 * 3.14 / 60 * rpm);
+    face->SetAngularVel(angular_velocity);
+}
+
+// Register this plugin with the simulator
+GZ_REGISTER_MODEL_PLUGIN(RouletteSpinner)
+}
---
+++ src/roulette_spinner.h
@@ -0,0 +1,31 @@
+#ifndef MARKER_DROPPER_H
+#define MARKER_DROPPER_H
+
+#include <ignition/math/Pose3.hh>
+
+#include <boost/bind.hpp>
+
+#include "gazebo/physics/physics.hh"
+#include "gazebo/common/common.hh"
+#include <gazebo/transport/transport.hh>
+#include "gazebo/gazebo.hh"
+
+#include <iostream>
+
+namespace gazebo
+{
+class RouletteSpinner: public ModelPlugin
+{
+  private:
+    event::ConnectionPtr updateConnection;
+    physics::ModelPtr roulette_wheel;
+    float rpm;
+
+  public:
+    RouletteSpinner() {}
+    ~RouletteSpinner() {}
+    void Load(physics::ModelPtr _parent, sdf::ElementPtr sdf);
+    void Update();
+};
+}
+#endif // MARKER_DROPPER_H
